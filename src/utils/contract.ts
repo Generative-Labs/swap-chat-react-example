692d2ce84419c6385fabb07d72a4939fcfc4c3cf
樊翔宇 <fanxiangyu66@163.com>
2022-06-01 15:13:39 +0800

    fix: update deps

--- src/utils/contract.ts
+++ src/utils/contract.ts
@@ -1,9 +1,9 @@
 const Web3 = require("web3");
 //@ts-ignore
-console.log(window.ethereum, "window.ethereum");
+// console.log(window.ethereum, "window.ethereum");
 //@ts-ignore
 const web3 = new Web3(window.ethereum);
-console.log(web3, "web3");
+// console.log(web3, "web3");
 
 export function GetERC20Contract(address: string) {
   const abi = require("../assets/abi/ERC20.json");
