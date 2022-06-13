c8035e6b42371bfbf5e7d7b9605ee85b16969bbb
zhaowei <zw204801@163.com>
2022-06-14 00:40:14 +0800

    feat: 添加页面响应式

--- src/App.tsx
+++ src/App.tsx
@@ -1,21 +1,22 @@
-import {
-  Chat,
-  Channel,
-  ChannelList,
-  CreateChannel,
-  DashBoard,
-  ContactList,
-} from 'web3-mq-react';
+import { Chat, Channel, CreateChannel, DashBoard, Main } from 'web3-mq-react';
 import { Web3MQ } from 'web3-mq';
-import ChannelHead from './components/ChannelHead';
 import './App.css';
 import 'web3-mq-react/dist/css/index.css';
 import ChannelInner from './components/ChannelInner';
 import Login from './components/Login';
 import useLogin from './hooks/useLogin';
+import {useEffect, useState} from 'react';
 
 const App = () => {
   const { signMetamask, token } = useLogin();
+  const [isMobile, setIsMobile] = useState(window.innerWidth <= 600);
+
+  useEffect(() => {
+    // 保证事件只挂载一次  避免重复render
+    window.addEventListener("resize", () => {
+      setIsMobile(window.innerWidth <= 600);
+    });
+  }, []);
 
   if (!token) {
     return <Login sign={signMetamask} />;
@@ -23,13 +24,9 @@ const App = () => {
 
   const client = Web3MQ.getInstance(token);
   return (
-    <Chat client={client}>
+    <Chat client={client} isMobile={isMobile}>
       <DashBoard />
-      <div className='channelContainer'>
-        <ChannelHead />
-        <ChannelList />
-        <ContactList />
-      </div>
+      <Main />
       <Channel>
         <CreateChannel />
         <ChannelInner />
