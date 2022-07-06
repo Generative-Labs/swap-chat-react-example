eb7f0688c87a1b48dcbc66c073ade6e55ee068f4
樊翔宇 <fanxiangyu66@163.com>
2022-07-06 12:04:40 +0800

    fix: update deps

--- src/App.tsx
+++ src/App.tsx
@@ -6,15 +6,20 @@ import 'web3-mq-react/dist/css/index.css';
 import ChannelInner from './components/ChannelInner';
 import Login from './components/Login';
 import useLogin from './hooks/useLogin';
+import { AppTypeEnum } from 'web3-mq-react/dist/types/types';
 
 const App = () => {
   const { signMetamask, token, logout } = useLogin();
-  const [isMobile, setIsMobile] = useState(window.innerWidth <= 600);
+  const [appType, setAppType] = useState<AppTypeEnum>(
+    window.innerWidth <= 600 ? AppTypeEnum['h5'] : AppTypeEnum['pc']
+  );
 
   useEffect(() => {
     // 保证事件只挂载一次  避免重复render
     window.addEventListener('resize', () => {
-      setIsMobile(window.innerWidth <= 600);
+      setAppType(
+        window.innerWidth <= 600 ? AppTypeEnum['h5'] : AppTypeEnum['pc']
+      );
     });
   }, []);
 
@@ -24,7 +29,7 @@ const App = () => {
 
   const client = Web3MQ.getInstance(token);
   return (
-    <Chat client={client} isMobile={isMobile} logout={logout}>
+    <Chat client={client} appType={appType} logout={logout}>
       <DashBoard />
       <Main />
       <Channel>
